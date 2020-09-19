c83e90d7e82ec601229db85496694c1154ca3388
Cooper <58396720+CoopJax@users.noreply.github.com>
2020-09-19 15:45:46 -0500

    Update Constants.cpp

--- Constants.cpp
+++ Constants.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+  // start defining the constants
+	const int LENGTH = 10;
+	const int WIDTH = 5;
+	const char NEWLINE = '\n'
+	int area
+
+	area = LENGTH * WIDTH
+	cout << area;
+	cout << NEWLINE;
+	return 0;
+}
