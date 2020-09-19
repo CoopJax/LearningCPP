94cc987c7412b278268ffe8c2cfe6fa376654bfa
Cooper <58396720+CoopJax@users.noreply.github.com>
2020-09-19 14:48:45 -0500

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,1 +1,17 @@
+/* This is a comment */
+/* Welcome to my c++ project! */
 
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins.
+int main() {
+   cout << "Hello World! :)"; // prints Hello World with a nice lil smiley face
+     cout << "Size of char : " << sizeof(char) << endl;
+   cout << "Size of int : " << sizeof(int) << endl;
+   cout << "Size of short int : " << sizeof(short int) << endl;
+   cout << "Size of long int : " << sizeof(long int) << endl;
+   cout << "Size of float : " << sizeof(float) << endl;
+   cout << "Size of double : " << sizeof(double) << endl;
+   cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+   return 0;
