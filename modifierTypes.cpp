82bba4988b1214a15c3eb6794bfe5a1f1b339c3c
Cooper <58396720+CoopJax@users.noreply.github.com>
2020-09-21 12:00:25 -0500

    Create modifierTypes.cpp

---
+++ modifierTypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+
+int main() {
+   short int i;           // short signed int
+   short unsigned int j;  // unsigned short int
+
+   j = 50000;
+
+   i = j;
+   cout << i << " " << j;
+
+   return 0;
+}
